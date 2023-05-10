bba2ca33abf7773797daae3a4c396c5f1193ffdb
Hans Cobian <hcobian90@gmail.com>
2023-05-10 10:37:16 -0600

    source code + some deployment test
    - source code + some deployment test

---
+++ NetApiK8s/Controllers/TestController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Testk8sDeployment.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private IConfiguration configuration;
+        public TestController(IConfiguration configuration)
+        {
+            this.configuration = configuration; 
+        }
+        // GET: api/<TestController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            return new OkObjectResult(
+                new {
+                    ASPNETCORE_ENVIRONMENT = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"),
+                    ENV_VAR_1 = Environment.GetEnvironmentVariable("ENV_VAR_1"),
+                    ENV_VAR_2 = Environment.GetEnvironmentVariable("ENV_VAR_2"),
+                    ENV_SECRET_1 = Environment.GetEnvironmentVariable("ENV_SECRET_1"),
+                    ENV_SECRET_2 = Environment.GetEnvironmentVariable("ENV_SECRET_2"),
+                    ConnectionStrings__postgres = configuration["ConnectionStrings:postgres"],
+                });
+        }
+    }
+}
+
